26b4d92842b15c06ad6f8c9a1d1e7f4c35e22955
somaya-77 <alkhtybsmytadl@gmail.com>
2026-01-08 01:14:27 +0200

    add first 3 sections

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,3 +1,11 @@
+import HeroSection from "./_features/HeroSection";
+import BenefitsSection from "./_features/BenefitsSection";
+import OccasionsSection from "./_features/OccasionsSection";
+
 export default function Home() {
-  return <></>;
+  return <div className="flex flex-col gap-10">
+    <HeroSection />
+    <OccasionsSection />
+    <BenefitsSection />
+  </div>;
 }
